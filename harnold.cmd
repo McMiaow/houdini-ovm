fbf880ca061359a6c7756ea4060af061b63bc63d
omar-vm <omarvm93@hotmail.com>
2021-03-13 21:06:55 -0500

    added windows render engine launchers

---
+++ harnold.cmd
@@ -0,0 +1,3 @@
+set RENDERER=arnold
+echo Using %RENDERER%
+"C:\Program Files\Side Effects Software\Houdini %HOUDINI_HTOA_VER%\bin\hindie.exe"
---
+++ hman.cmd
@@ -0,0 +1,3 @@
+set RENDERER=renderman
+echo Using %RENDERER%
+"C:\Program Files\Side Effects Software\Houdini %HOUDINI_RFH_VER%\bin\hindie.exe"
